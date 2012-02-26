85d7ebc982ef3c45779a59bde9dc5d6a35d47996
Hao Lian <me@haolian.org>
2012-02-26 02:50:29 -0500

    Explosion.hs: refactor into a Connection type.

--- src/Network/Protocol/Explosion.hs
+++ src/Network/Protocol/Explosion.hs
@@ -4,18 +4,28 @@ import Control.Concurrent (forkIO, killThread)
 import Control.Exception (try)
 import Control.Exception.Base (throwIO)
 import Control.Monad.IO.Class (liftIO)
-import Control.Monad.Trans.Resource (runResourceT, with)
+import Control.Monad.Trans.Resource (release, runResourceT, with)
 import qualified Crypto.Random.AESCtr as AESCtr
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as BC
 import qualified Data.ByteString.Lazy as L
 import Data.Conduit
 import Data.Conduit.Binary (sinkHandle, sourceHandle)
+import Debug.Trace
 import Network (PortID (..))
 import qualified Network
 import qualified Network.TLS as TLS
 import Network.TLS.Extra (ciphersuite_all)
 import System.IO
+
+data Connection = Connection {
+  connHandle :: Handle,
+  connSource :: Source IO ByteString,
+  connSink :: Sink ByteString IO ()
+  }
+
+isClosed conn = hIsEOF $ connHandle conn
 
 tlsParams = TLS.defaultParams { TLS.pCiphers = ciphersuite_all }
 
@@ -25,10 +35,10 @@ tee obj f = do
   f x
   return x
 
-lineBuffer = flip hSetBuffering $ LineBuffering
+noBuffer = flip hSetBuffering $ NoBuffering
 
 connectTo host port =
-    Network.connectTo host (PortNumber $ fromIntegral port) `tee` lineBuffer
+    Network.connectTo host (PortNumber $ fromIntegral port) `tee` noBuffer
 
 source :: IO ByteString -> Source IO ByteString
 source recv = Source pull close
@@ -48,23 +58,44 @@ sink send = SinkData push close
       push bs = (liftIO $ send bs) >> (return $ Processing push close)
       close = return ()
 
+securePlumbing :: String -> Int -> ResourceT IO Connection
 securePlumbing host port = do
   prg <- liftIO $ AESCtr.makeSystem
-  conn <- connectTo host port
-  client <- handshake prg conn
-  return (source $ TLS.recvData client,
-          sink $ TLS.sendData client . L.fromChunks . (:[]))
-    where
-      handshake prg conn = TLS.client tlsParams prg conn `tee` TLS.handshake
+  (releaseHandle, handle) <- with (connectTo host port) hClose
+  (releaseContext, context) <- with (TLS.client tlsParams prg handle) TLS.bye
+  TLS.handshake context
+  return $ Connection handle
+    (source $ TLS.recvData context)
+    (sink $ TLS.sendData context . L.fromChunks . (:[]))
 
+plumbing :: String -> Int -> ResourceT IO Connection
 plumbing host port =  do
-  conn <- connectTo host port
-  return (source $ B.hGet conn 4096, sink $ B.hPutStr conn)
+  (releaseHandle, handle) <- with (connectTo host port) hClose
+  return $ Connection handle
+    (source $ B.hGet handle 4096)
+    (sink $ B.hPutStr handle)
+
+readLine handle = liftIO $ do
+  eof <- hIsEOF handle
+  if eof then return IOClosed else fmap IOOpen $ hGetLine handle
+
+stdinSource = Source pull close
+    where
+      pull = liftIO $ do
+        line <- hGetLine stdin
+        if line == "send\n" then return Closed else return $ Open stdinSource (BC.pack line)
+      close = return ()
+
+telnet' :: Connection -> ResourceT IO ()
+telnet' conn  = do
+  trace "read" $ return ()
+  stdinSource $$ connSink conn
+  trace "write" $ return ()
+  connSource conn $$ sinkHandle stdout
+  telnet' conn
 
 telnet host port =
     runResourceT $ do
-      (source, sink) <- liftIO $ (if port == 443 then securePlumbing else plumbing) host port
-      liftIO $ mapM_ lineBuffer [stdin, stdout]
-      (releaseThread, _) <-
-          with (forkIO $ runResourceT $ sourceHandle stdin $$ sink) killThread
-      source $$ sinkHandle stdout
+      conn <- (if port == 443 then securePlumbing else plumbing) host port
+      liftIO $ mapM_ noBuffer [stdin, stdout]
+      telnet' conn
